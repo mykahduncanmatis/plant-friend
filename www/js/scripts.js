d1f874b15381ecc011835f4ed3d7042154f33814
mykahduncanmatis <mykah.duncanmatis@uleth.ca>
2022-10-17 11:34:51 -0600

    initial commit

--- www/js/scripts.js
+++ www/js/scripts.js
@@ -5,3 +5,18 @@ var app = new Framework7({
 });
 var mainView = app.views.create('.view-main')
 
+// by default:
+// - plants starts healthy
+// - dry out over time
+// - deplete in nutrients over time
+
+// interactions:
+// - water it, replenish it
+// - feed it, nutrients for the plant
+// - trim it
+
+// care: 
+// - too much water, plant near death
+// - too much fertilizer, plant near death
+// - if plant falls into near death state, you can only heal it by trimming it
+

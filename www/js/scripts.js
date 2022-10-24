7097109dd3e2bdb3cd4fdbe3efb875849022ea5f
mykahduncanmatis <mykah.duncanmatis@uleth.ca>
2022-10-24 11:21:41 -0600

    added to js

--- www/js/scripts.js
+++ www/js/scripts.js
@@ -5,6 +5,29 @@ var app = new Framework7({
 });
 var mainView = app.views.create('.view-main')
 
+var waterlevel = 20;
+var nowater = 0;
+var drowning = 40;
+
+var neardeath = false //starts healthy so near death is false. This is boolean
+
+function dryout() {
+  waterlevel--; //every time this func runs, water level decreases by 1
+  console.log(waterlevel)
+  checkhealth();
+  var watertimer = setTimeout(dryout,500); // for timing (example on w3 schools setTimeout()) in milliseconds
+}
+
+dryout() //call function after it is declared
+
+function checkhealth() {
+  if(waterlevel <= nowater)  {
+    neardeath = true;
+    console.log("help!")
+    $("#plant path").css("fill", "#D2691E")
+  }
+}
+
 // by default:
 // - plants starts healthy
 // - dry out over time
